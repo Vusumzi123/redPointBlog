e4be784ce304194bd9f54609fbfbe8bd5f78142f
Vusuzmzi <D.O.GeeVz@gmail.com>
2018-07-02 18:49:59 -0500

    added mail register functionality and code optimizations

--- App.js
+++ App.js
@@ -3,6 +3,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const path = require('path');
 const cors = require('cors')
+const hogan = require('hogan-express');
 
 const routes = require('./routes.js');
 
@@ -12,6 +13,8 @@ const App = function () {
     this.app = express();
 
     this.middleware = function () {
+        this.app.engine('html', hogan);
+        this.app.set('view engine', 'html');
         this.app.use(cors());
         this.app.use(express.static(path.join(__dirname + '/dist')));
         this.app.use(bodyParser.urlencoded({ extended: false }));
